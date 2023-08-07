6eb470a1946f995f85ea9ded65de581278c86832
Fredrik Uddenfeldt <fredrikuddenfeldt@fredriks-air.lan>
2023-08-07 22:25:35 +0200

    romulus content

--- script.js
+++ script.js
@@ -1,49 +1,50 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let menuTitles = document.querySelectorAll(".menu-title");
-    
-    menuTitles.forEach(title => {
-        title.addEventListener("click", function() {
-            let content = this.nextElementSibling;
-            if (content.style.display === "none" || content.style.display === "") {
-                content.style.display = "block";
-                localStorage.setItem(this.textContent, 'open');
-            } else {
-                content.style.display = "none";
-                localStorage.removeItem(this.textContent);
-            }
+
+    // Fetch and inject header.html
+    fetch("/header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector("#headerContainer").innerHTML = data;
+
+            // Once the header is loaded, attach event listeners to the menu titles
+            let menuTitles = document.querySelectorAll(".menu-title");
+            menuTitles.forEach(title => {
+                title.addEventListener("click", function() {
+                    let content = this.nextElementSibling;
+                    if (content.style.display === "none" || content.style.display === "") {
+                        content.style.display = "block";
+                        localStorage.setItem(this.textContent, 'open');
+                    } else {
+                        content.style.display = "none";
+                        localStorage.removeItem(this.textContent);
+                    }
+                });
+
+                // Restore menu state from local storage
+                if (localStorage.getItem(title.textContent)) {
+                    title.nextElementSibling.style.display = "block";
+                }
+            });
         });
 
-        // Restore menu state from local storage
-        if (localStorage.getItem(title.textContent)) {
-            title.nextElementSibling.style.display = "block";
-        }
-    });
+    // Fetch and inject footer.html
+    fetch("/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector("#footerContainer").innerHTML = data;
+        });
 
     // AJAX-style content loading (using Fetch API)
-    let links = document.querySelectorAll(".menu-content a");
-    links.forEach(link => {
-        link.addEventListener('click', function(e) {
+    document.querySelectorAll('.menu-content a').forEach(link => {
+        link.addEventListener('click', async function(e) {
             e.preventDefault();
-            fetch(this.href)
-                .then(response => response.text())
-                .then(data => {
-                    // Assuming you have a content container to display the article
-                    document.querySelector('#contentContainer').innerHTML = data;
-                });
+
+            const href = this.getAttribute('href');
+
+            const response = await fetch(href);
+            const content = await response.text();
+
+            document.getElementById('contentContainer').innerHTML = content;
         });
     });
 });
-
-document.querySelectorAll('.menu-content a').forEach(link => {
-    link.addEventListener('click', async function(e) {
-        e.preventDefault();
-
-        const href = this.getAttribute('href');
-        
-        const response = await fetch(href);
-        const content = await response.text();
-
-        document.getElementById('contentContainer').innerHTML = content;
-    });
-});
-
